e36050a009f216b95eaf4a6bae44203bdcb6db94
bmerchant22 <merchantburhanuddin484@gmail.com>
2023-12-08 02:19:34 +0530

    Fix app cards

--- src/Apps.jsx
+++ src/Apps.jsx
@@ -6,6 +6,8 @@ import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import TextField from '@mui/material/TextField';
 import { Box, Button } from '@mui/material';
+import AppCard from './HomeAppCard';
+import PendingAppCard from '@/PendingAppCard'; // Import PendingAppCard
 import StoreAppCard from '@/StoreAppCard';
 const installedAppsData = [
     {
@@ -13,15 +15,13 @@ const installedAppsData = [
         description: 'Code editor',
         image: 'https://imgs.search.brave.com/i_x3Xj7berzbEMNffR4YncVE-AcMw4MHEn6bVCps96c/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9sb2dv/dHlwLnVzL2ZpbGUv/dnMtY29kZS5zdmc.svg',
     },
-    {
-        name: 'Figma',
-        description: 'Designing app',
-        image: 'https://imgs.search.brave.com/FdIGGfc3R9dZX9ggCvuTLVjuAb0LfOkNMSxiNmq0NrE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9sb2dv/dHlwLnVzL2ZpbGUv/ZmlnbWEuc3Zn.svg',
-    },
 ];
 const pendingAppsData = [
-// Pending Apps data
-// Add your pending apps data here
+  {
+    name: 'Figma',
+    description: 'Designing app',
+    image: 'https://imgs.search.brave.com/FdIGGfc3R9dZX9ggCvuTLVjuAb0LfOkNMSxiNmq0NrE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9sb2dv/dHlwLnVzL2ZpbGUv/ZmlnbWEuc3Zn.svg',
+  },
 ];
 const globalAppsData = [
     {
@@ -64,50 +64,78 @@ const AppGrid = () => {
                 return [];
         }
     };
-    return (<div style={{
+
+    const getActiveSectionComponent = (section = curr) => {
+        switch (section) {
+            case 'Installed':
+                return AppCard; // Use StoreAppCard for 'Installed' tab
+            case 'Pending':
+                return PendingAppCard; // Use PendingAppCard for 'Pending' tab
+            case 'Global':
+                return StoreAppCard; // Use HomeAppCard for 'Global' tab
+            default:
+                return null; // Return null or a default component for unknown tabs
+        }
+    };
+
+    const ActiveCardComponent = getActiveSectionComponent();
+
+    return (
+        <div style={{
             backgroundColor: "#f6f6f6",
             minHeight: '100vh'
         }}>
-      <Container maxWidth="xl">
-        <Box border="1px solid #ddd" boxShadow={"0px 0px 5px 0px #D1D1D1"} borderRadius="10px" p={2} sx={{ backgroundColor: "#FBF8F8" }}>
-          
-           <Grid item xs={12}>
-              <Box mb={2} display="flex" justifyContent="center">
-                {/* Section Tabs */}
-                <Button variant={curr === 'Installed' ? 'contained' : 'outlined'} onClick={() => handleTabChange({}, 'Installed')} sx={{ borderRadius: '16px 0 0 16px', flex: 1 }}>
-                  Installed
-                </Button>
-                <Button variant={curr === 'Pending' ? 'contained' : 'outlined'} onClick={() => handleTabChange({}, 'Pending')} sx={{ flex: 1 }}>
-                  Pending
-                </Button>
-                <Button variant={curr === 'Global' ? 'contained' : 'outlined'} onClick={() => handleTabChange({}, 'Global')} sx={{ borderRadius: '0 16px 16px 0', flex: 1 }}>
-                  Global
-                </Button>
-              </Box>
-            </Grid>
-            <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Box mb={2} display="flex" justifyContent="flex-start">
-                <form onSubmit={handleSearchSubmit} style={{ display: 'flex' }}>
-                  <div style={{ position: 'relative' }}>
-                    <IconButton type="submit" sx={{ p: '10px' }}>
-                      <SearchIcon />
-                    </IconButton>
-                    <TextField label="Search..." value={searchValue} onChange={handleSearchChange} sx={{ ml: 1, width: '200px' }}/>
-                  </div>
-                </form>
-              </Box>
-            </Grid>
-            <Grid item xs={12}>
-              <Grid container spacing={2}>
-                {filteredCards.map((card, index) => (<Grid item key={index} xs={12} sm={6} md={4}>
-                    <StoreAppCard linkTo={`/student/app?id=${card.name}`} name={card.name} description={card.description} image={card.image}/>
-                  </Grid>))}
-              </Grid>
-            </Grid>
-          </Grid>
-        </Box>
-      </Container>
-    </div>);
+            <Container maxWidth="xl">
+                <Box border="1px solid #ddd" boxShadow={"0px 0px 5px 0px #D1D1D1"} borderRadius="10px" p={2} sx={{ backgroundColor: "#FBF8F8" }}>
+                    <Grid item xs={12}>
+                        <Box mb={2} display="flex" justifyContent="center">
+                            {/* Section Tabs */}
+                            <Button variant={curr === 'Installed' ? 'contained' : 'outlined'} onClick={() => handleTabChange({}, 'Installed')} sx={{ borderRadius: '16px 0 0 16px', flex: 1 }}>
+                                Installed
+                            </Button>
+                            <Button variant={curr === 'Pending' ? 'contained' : 'outlined'} onClick={() => handleTabChange({}, 'Pending')} sx={{ flex: 1 }}>
+                                Pending
+                            </Button>
+                            <Button variant={curr === 'Global' ? 'contained' : 'outlined'} onClick={() => handleTabChange({}, 'Global')} sx={{ borderRadius: '0 16px 16px 0', flex: 1 }}>
+                                Global
+                            </Button>
+                        </Box>
+                    </Grid>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Box mb={2} display="flex" justifyContent="flex-start">
+                                <form onSubmit={handleSearchSubmit} style={{ display: 'flex' }}>
+                                    <div style={{ position: 'relative' }}>
+                                        <IconButton type="submit" sx={{ p: '10px' }}>
+                                            <SearchIcon />
+                                        </IconButton>
+                                        <TextField label="Search..." value={searchValue} onChange={handleSearchChange} sx={{ ml: 1, width: '200px' }} />
+                                    </div>
+                                </form>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Grid container spacing={2}>
+                                {filteredCards.map((card, index) => (
+                                    <Grid item key={index} xs={12} sm={6} md={4}>
+                                        {/* Use the dynamically determined card component with explicit props */}
+                                        {ActiveCardComponent === AppCard && (
+                                            <AppCard linkTo={`/student/app?id=${card.name}`} name={card.name} description={card.description} image={card.image} />
+                                        )}
+                                        {ActiveCardComponent === PendingAppCard && (
+                                            <PendingAppCard linkTo={`/student/app?id=${card.name}`} name={card.name} description={card.description} image={card.image} />
+                                        )}
+                                        {ActiveCardComponent === StoreAppCard && (
+                                            <StoreAppCard linkTo={`/student/app?id=${card.name}`} name={card.name} description={card.description} image={card.image} />
+                                        )}
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
+        </div>
+    );
 };
 export default AppGrid;
--- src/pages/student/dashboard.jsx
+++ src/pages/student/dashboard.jsx
@@ -3,38 +3,50 @@ import React, { useState, useEffect } from 'react';
 import Navbar from '@/Navbar';
 import AppGrid from '@/Apps';
 import Dashboard from '@/Home';
-import Chat from '@/chat';
 
 const StudentDashboard = () => {
   const [curr, setCurr] = useState('Dashboard');
 
   const handleTabChange = (event, newValue) => {
-      setCurr(newValue);
+    setCurr(newValue);
   };
 
   useEffect(() => {
-      const handleMessage = (event) => {
-          if (event.data.event === 'login-with-token') {
-              const { loginToken } = "O7fvjGqoVI5d0RTLI_7-8AxVajWX_IrvBvg3Ryt7kPQ";
-              console.log('Received login token:', loginToken);
-          }
-      };
-      window.addEventListener('message', handleMessage);
-      return () => {
-          window.removeEventListener('message', handleMessage);
-      };
+    const handleMessage = (event) => {
+      if (event.data.event === 'login-with-token') {
+        const { loginToken } = event.data;
+        console.log('Received login token:', loginToken);
+        // You can use the loginToken for further authentication or other actions
+      }
+    };
+    window.addEventListener('message', handleMessage);
+
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
   }, []);
 
   return (
-      <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-          <Navbar currentTab={curr} onTabChange={handleTabChange} />
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <Navbar currentTab={curr} onTabChange={handleTabChange} />
 
-          <div style={{ flex: 1, overflow: 'hidden' }}>
-              {curr === 'Chat' ? <Chat />:""}
-              {curr === 'Dashboard' ? <Dashboard /> : ""}
-              {curr === 'Apps' ? <AppGrid /> : ""}
-          </div>
+      <div style={{ flex: 1, overflow: 'hidden' }}>
+        {curr === 'Dashboard' ? <Dashboard /> : ''}
+        {curr === 'Apps' ? <AppGrid /> : ''}
+
+        {/* Render Rocket.Chat chat interface using iframe */}
+        {curr === 'Chat' ? (
+          <iframe
+            src="http://localhost:3000/channel/general" // Update the URL as needed
+            width="100%"
+            height="100%"
+            title="Rocket.Chat"
+          ></iframe>
+        ) : (
+          ''
+        )}
       </div>
+    </div>
   );
 };
 
