5b129c631711c8a224c71ad014ef129e00dc91bf
Deven Gangwani <devengangwani12@gmail.com>
2023-12-09 03:26:00 +0530

    Shared config files

--- student/next.config.js
+++ student/next.config.js
@@ -1,12 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
-  images: { unoptimized: true},
-  webpack: (config, { buildId, dev }) => {
-    config.resolve.symlinks = false
-    return config
-  }
-}
-
-module.exports = nextConfig
+../next.config.js
