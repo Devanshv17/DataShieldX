c10187d4ea85b4da4306d389cec6f5b9523cba55
bmerchant22 <merchantburhanuddin484@gmail.com>
2023-12-16 01:35:47 +0530

    Chat to be opened in different tab

--- student/shared/chat.jsx
+++ student/shared/chat.jsx
@@ -4,7 +4,13 @@ import {useRouter} from 'next/router';
 export default function Chat() {
     const router = useRouter();
     useEffect(() => {
-        router.push(`${process.env.NEXT_PUBLIC_CHAT_SERVER}`)
+        // router.push(`${process.env.NEXT_PUBLIC_CHAT_SERVER}`)
+        const newWindow = window.open(`${process.env.NEXT_PUBLIC_CHAT_SERVER}`, '_blank');
+
+        // Optional: You can focus on the new window if needed
+        if (newWindow) {
+            newWindow.focus();
+        }
     })
     return(
         <div /> 
